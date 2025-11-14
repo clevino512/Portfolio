57f1acb2a24d2e60473e69b0a0a32dd629f91ae5
LaMa-Log <ton-email@exemple.com>
2025-11-14 12:07:41 +0300

    Prete

--- frontend/src/components/header/footer.tsx
+++ frontend/src/components/header/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full border-t border-gray-700 bg-gray-900 py-4 text-center text-xs text-gray-400">
       <p>
-        © 2025 — <span className="text-sm font-semibold text-gray-100">Mahiratra Groupe</span>. Tous droits réservés.
+        © 2025 — <span className="text-sm font-semibold text-gray-100">RABENANTENAINA Clévin</span>. Tous droits réservés.
       </p>
     </footer>
   )
