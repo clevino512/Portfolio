4d286e1f41a6f5c0c9affd55a93615159036ea54
clevino512 <clevino512@gmail.com>
2026-05-11 23:36:22 +0200

    fix(contact): update email submission parameters to include timestamp

--- frontend/src/components/body/contact.tsx
+++ frontend/src/components/body/contact.tsx
@@ -58,9 +58,9 @@ export default function Contact() {
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         {
-          from_name: formData.name,
-          from_email: formData.email,
-          subject: formData.subject,
+          name: formData.name,
+          email: formData.email,
+          time: new Date().toLocaleString(),
           message: formData.message,
         },
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
